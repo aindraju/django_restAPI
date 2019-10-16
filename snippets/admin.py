5287055eb416f5bb9000a556cf139c87c42fd037
Armando <aindraju@gmail.con>
2019-10-16 10:47:52 +0200

    adding owner/user

--- snippets/admin.py
+++ snippets/admin.py
@@ -2,10 +2,9 @@ from django.contrib import admin
 from .models import Snippet
 
 
-
-
 class SnippetAdmin(admin.ModelAdmin):
     readonly_fields = ('highlighted',)
 
+
 # Register your models here.
-admin.site.register(Snippet,SnippetAdmin)
+admin.site.register(Snippet, SnippetAdmin)
